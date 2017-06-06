dc90159fdd0c24cbf8c6473671c3bf2be1b5d24c
fangzhoui <601064569@qq.com>
2017-06-06 16:35:50 +0800

    change drop.js

--- drop.js
+++ drop.js
@@ -2,7 +2,11 @@ function drop (){
 	var el = document.getElementById("ap")
 	var disX = 0
 	var disY = 0
-	el.onmousedown = function (){
-		disX = el.clearInterval()
+	el.onmousedown = function (ev){
+		disX = ev.pageX - el.offsetLeft
+		disY = ev.pageY - el.offsetTop
+		document.onmouseup = function (ev){
+			el.style.left = disX
+		}
 	}
 }
